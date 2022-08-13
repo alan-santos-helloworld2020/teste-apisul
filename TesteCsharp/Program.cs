d9d06675c6361054a611320ab52836a19c86123c
Alan Paulo dos santos <onloadtecnologia@gmail.com>
2022-08-12 21:30:11 -0300

    listando o percentual de uso do elevador

--- TesteCsharp/Program.cs
+++ TesteCsharp/Program.cs
@@ -12,18 +12,48 @@ namespace TesteCsharp
             ElevadorService servico = new ElevadorService();
             LeitorService leitor = new LeitorService();
 
-            Console.WriteLine("Andares Menos frequentado");
+            Console.WriteLine("Lista de andares ordenada por frequencia");
             servico.andarMenosUtilizado();
-            
+
+            Console.WriteLine("\n\n");
+
             Console.WriteLine("Elevador Mais frequentado");
             servico.elevadorMaisFrequentado();
+
+            Console.WriteLine("\n\n");
 
             Console.WriteLine("Elevador Menos frequentado");
             servico.elevadorMenosFrequentado();
 
+            Console.WriteLine("\n\n");
+
             Console.WriteLine("periodo de maior utilização do conjunto de elevadores");
             servico.periodoMaiorUtilizacaoConjuntoElevadores();
 
+            Console.WriteLine("\n");
+
+            Console.WriteLine("percentual de uso elevador A");
+            servico.percentualDeUsoElevadorA();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("percentual de uso elevador B");
+            servico.percentualDeUsoElevadorB();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("percentual de uso elevador C");
+            servico.percentualDeUsoElevadorC();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("percentual de uso elevador D");
+            servico.percentualDeUsoElevadorD();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("percentual de uso elevador E");
+            servico.percentualDeUsoElevadorE();
 
         }
     }
--- TesteCsharp/services/ElevadorService.cs
+++ TesteCsharp/services/ElevadorService.cs
@@ -26,7 +26,7 @@ namespace TesteCsharp.services
             var elev = elevador.OrderByDescending(x=>x.Count()).ToList();
             foreach (var item in elev)
             {
-                Console.WriteLine($"elevador = {item.Key}");
+                Console.WriteLine($"elevador = {item.Key} | frequencia = {item.Count()}");
                 keyElevador.Add(item.Key);
             }
             return keyElevador;
@@ -39,7 +39,7 @@ namespace TesteCsharp.services
             var elev = elevador.OrderBy(x=>x.Count()).ToList();
             foreach (var item in elev)
             {
-                Console.WriteLine($"elevador = {item.Key}");
+                Console.WriteLine($"elevador = {item.Key} | frequencia = {item.Count()}");
                 keyElevador.Add(item.Key);
             }
 
@@ -48,27 +48,47 @@ namespace TesteCsharp.services
 
         public float percentualDeUsoElevadorA()
         {
-            throw new NotImplementedException();
+            float tmUsoGeral = leitor.findAll().Count();
+            float qtUseElevador = leitor.findAll().Where(x=>x.elevador.ToString()=="A").Count(); 
+            float resp = (100 / tmUsoGeral) * qtUseElevador;
+            Console.WriteLine(resp);
+            return resp;            
         }
 
         public float percentualDeUsoElevadorB()
         {
-            throw new NotImplementedException();
+            float tmUsoGeral = leitor.findAll().Count();
+            float qtUseElevador = leitor.findAll().Where(x=>x.elevador.ToString()=="B").Count(); 
+            float resp = (100 / tmUsoGeral) * qtUseElevador;
+            Console.WriteLine(resp);
+            return resp;  
         }
 
         public float percentualDeUsoElevadorC()
         {
-            throw new NotImplementedException();
+            float tmUsoGeral = leitor.findAll().Count();
+            float qtUseElevador = leitor.findAll().Where(x=>x.elevador.ToString()=="C").Count(); 
+            float resp = (100 / tmUsoGeral) * qtUseElevador;
+            Console.WriteLine(resp);
+            return resp;  
         }
 
         public float percentualDeUsoElevadorD()
         {
-            throw new NotImplementedException();
+            float tmUsoGeral = leitor.findAll().Count();
+            float qtUseElevador = leitor.findAll().Where(x=>x.elevador.ToString()=="D").Count(); 
+            float resp = (100 / tmUsoGeral) * qtUseElevador;
+            Console.WriteLine(resp);
+            return resp;  
         }
 
         public float percentualDeUsoElevadorE()
         {
-            throw new NotImplementedException();
+            float tmUsoGeral = leitor.findAll().Count();
+            float qtUseElevador = leitor.findAll().Where(x=>x.elevador.ToString()=="E").Count(); 
+            float resp = (100 / tmUsoGeral) * qtUseElevador;
+            Console.WriteLine(resp);
+            return resp;  
         }
 
         public List<char> periodoMaiorFluxoElevadorMaisFrequentado()
@@ -80,11 +100,10 @@ namespace TesteCsharp.services
         {
             List<char> lisTurno = new List<char>();
             var turno = from p in leitor.findAll() group p by p.turno into SelectTurno select SelectTurno;
-
             var pe = turno.OrderByDescending(x=>x.Count()).ToList();
             foreach (var item in pe)
             {
-               Console.WriteLine($"periodo = {item.Key}");
+               Console.WriteLine($"periodo = {item.Key} | total= {item.Count()}");
                lisTurno.Add(item.Key);
 
             }
